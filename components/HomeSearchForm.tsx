7b344fc6896779547007710e5c34f6abc0c51bbf
Emily Daitch <emily.daitch@gmail.com>
2023-05-01 20:24:12 -0400

    gridded home search form

--- components/HomeSearchForm.tsx
+++ components/HomeSearchForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography, Paper, Box } from "@mui/material";
+import { Button, Typography, Paper, Box, Grid } from "@mui/material";
 import { FormProvider, useForm } from "react-hook-form";
 import {useRouter} from 'next/router'
 import { FormInputText } from "./form-components/FormInputText";
@@ -7,12 +7,6 @@ import { FormInputDropdown } from "./form-components/FormInputDropdown";
 import { FormInputSlider } from "./form-components/FormInputSlider";
 
 const defaultValues = {
-  propertyType: "House",
-  cityZip: "",
-  beds: "2",
-  baths: "2",
-  minPrice: "100000",
-  maxPrice: "200000",
 };
 
 interface IFormInput {
@@ -45,42 +39,69 @@ export const HomeSearchForm = () => {
                 alignItems: "center",
                 border: "none"
             }}>
-                <FormInputDropdown name="propertyType"control={control} label="Property Type" options={[
-                  {
-                    label: "House",
-                    value: "House"
-                  },
-                  {
-                    label: "Apartment",
-                    value: "Apartment"
-                  }
-                ]}/>
-                <FormInputText name="cityZip" control={control} label="City / Zip" />
-                <FormInputDropdown name="beds"control={control} label="Beds" options={[
-                  {
-                    label: "0",
-                    value: "0"
-                  },
-                  {
-                    label: "1",
-                    value: "1"
-                  },
-                  {
-                    label: "2",
-                    value: "2"
-                  },
-                  {
-                    label: "3",
-                    value: "3"
-                  },
-                  {
-                    label: "4+",
-                    value: "4+"
-                  }
-                ]}/>
-                <FormInputText name="baths" control={control} label="Baths" />
-                <FormInputText name="minPrice" control={control} label="Min. Price" />
-                <FormInputText name="maxPrice" control={control} label="Max. Price" />
+              <Grid container sx={{justifyContent: 'center'}}>
+                <Grid item key={'propertyType'} xs={12} md={6} lg={2}>
+                  <FormInputDropdown name="propertyType" control={control} label="Property Type" options={[
+                    {
+                      label: "House",
+                      value: "House"
+                    },
+                    {
+                      label: "Apartment",
+                      value: "Apartment"
+                    }
+                  ]}/>
+                </Grid>
+                <Grid item key={'cityZip'} xs={12} md={6} lg={2}>
+                  <FormInputText name="cityZip" control={control} placeholder="City / Zip" />
+                </Grid>
+                <Grid item key={'beds'} xs={12} md={6} lg={2}>
+                  <FormInputDropdown name="beds"control={control} label="Beds" options={[
+                    {
+                      label: "1",
+                      value: "1"
+                    },
+                    {
+                      label: "2",
+                      value: "2"
+                    },
+                    {
+                      label: "3",
+                      value: "3"
+                    },
+                    {
+                      label: "4+",
+                      value: "4+"
+                    }
+                  ]}/>
+                </Grid>
+                <Grid item key={'baths'} xs={12} md={6} lg={2}>
+                  <FormInputDropdown name="baths" control={control} label="Baths" options={[
+                    {
+                      label: "1",
+                      value: "1"
+                    },
+                    {
+                      label: "2",
+                      value: "2"
+                    },
+                    {
+                      label: "3",
+                      value: "3"
+                    },
+                    {
+                      label: "4+",
+                      value: "4+"
+                    }
+                  ]}/>
+                </Grid>
+                <Grid item key={'minPrice'} xs={12} md={6} lg={2}>
+                  <FormInputText name="minPrice" control={control} placeholder="Min. Price" />
+                </Grid>
+                <Grid item key={'maxPrice'} xs={12} md={6} lg={2}>
+                  <FormInputText name="maxPrice" control={control} placeholder="Max. Price" />
+                </Grid>
+              </Grid>
             </Box>
             <br/>
             <Box sx={{
