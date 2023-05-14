65e4a15f6fa831db7efed030e0c8225f0a227b2e
Emily Daitch <emily.daitch@gmail.com>
2023-05-14 10:08:47 -0400

    add tabs to home results page

--- pages/searchResult/searchResult.tsx
+++ pages/searchResult/searchResult.tsx
@@ -1,9 +1,62 @@
 import Link from "next/link";
 import React, { useState, useEffect } from 'react';
-import { Box, ImageList, ImageListItem, Paper } from "@mui/material";
+import { Box, ImageList, ImageListItem, Paper, Tab, Tabs, Typography } from "@mui/material";
+import { url } from "inspector";
+import primaryHomePhoto from "../../public/primaryhomephoto.jpg"
+
+type PropertyData = {
+    address: string,
+    lastSoldPrice: number,
+    askPrice: number,
+    sqFt: number,
+    beds: number,
+    baths: number,
+    yearBuilt: number,
+    lotSize: number,
+    predictedValue: number,
+    media: string[]
+}
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+  }
+  
+  function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }} >
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
 
 const SearchResult: React.FC = () => {
     const [isMobile, setIsMobile] = useState(false);
+    const [tab, setTab] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newTab: number) => {
+      setTab(newTab);
+    };
     useEffect(() => {
         const checkIsMobile = () => {
           setIsMobile(window.innerWidth <= 768);
@@ -35,23 +88,13 @@ const SearchResult: React.FC = () => {
             }
         ]
     );
-    const [selectedProperty, setSelectedProperty] = useState({
-        address: '',
-        lastSoldPrice: 0,
-        askPrice: 0,
-        sqFt: 0,
-        beds: 0,
-        baths: 0,
-        yearBuilt: 2000,
-        lotSize: 0,
-        predictedValue: 0,
-        media: ['']
-    });
+    const [selectedProperty, setSelectedProperty] = useState<PropertyData | null>(null);
 
     useEffect(() => {
         const data = localStorage.getItem('propertyData');
         if (data) {
         setPropertyData(JSON.parse(data));
+        setSelectedProperty(JSON.parse(data)[0]);
         }
     }, []);
 
@@ -117,6 +160,13 @@ return (
         {/* Detailed information and images */}
         {selectedProperty && (
         <div style={{ flex: 2, marginLeft: isMobile ? 0 : 16 }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={tab} onChange={handleChange} aria-label="basic tabs example">
+                <Tab label="Info" {...a11yProps(0)} />
+                <Tab label="Photos" {...a11yProps(1)} />
+            </Tabs>
+        </Box>
+        <TabPanel value={tab} index={0}>
             <p>Address: {selectedProperty.address}</p>
             <p>Price: {selectedProperty.askPrice}</p>
             <p>SqFt: {selectedProperty.sqFt}</p>
@@ -124,7 +174,33 @@ return (
             <p>Baths: {selectedProperty.baths}</p>
             <p>Year Built: {selectedProperty.yearBuilt}</p>
             <p>Lot Size: {selectedProperty.lotSize}</p>
+        </TabPanel>
+        <TabPanel value={tab} index={1}>
             <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                {selectedProperty.media.map((url, index) => (
+                //   <img src={url} key={index} alt={`Property at ${selectedProperty.address}`} />
+
+                <ImageListItem key={index}>
+                <img
+                    src={`${url}?w=164&h=164&fit=crop&auto=format`}
+                    srcSet={`${url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                    alt={'home image'}
+                    loading="lazy"
+                />
+                </ImageListItem>
+                ))}
+            </ImageList>
+        </TabPanel>
+        {/* {selectedProperty && (
+        <div style={{ flex: 2, marginLeft: isMobile ? 0 : 16 }}> */}
+            {/* <p>Address: {selectedProperty.address}</p>
+            <p>Price: {selectedProperty.askPrice}</p>
+            <p>SqFt: {selectedProperty.sqFt}</p>
+            <p>Beds: {selectedProperty.beds}</p>
+            <p>Baths: {selectedProperty.baths}</p>
+            <p>Year Built: {selectedProperty.yearBuilt}</p>
+            <p>Lot Size: {selectedProperty.lotSize}</p> */}
+            {/* <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
             {selectedProperty.media.map((url, index) => (
             //   <img src={url} key={index} alt={`Property at ${selectedProperty.address}`} />
 
@@ -137,7 +213,7 @@ return (
             />
             </ImageListItem>
             ))}
-            </ImageList>
+            </ImageList> */}
         </div>
         )}
     </div>
