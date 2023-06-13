00e097578b45f20dd2d87e219bca93f8382c7e62
Emily Daitch <emily.daitch@gmail.com>
2023-06-12 21:38:50 -0400

    next config for images from test server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    domains: ['dvvjkgh94f2v6.cloudfront.net']
+  },
 }
 
 module.exports = nextConfig
