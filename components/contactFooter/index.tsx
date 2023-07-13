81a8ead55dda64a8ccf2e1a86f8b8f3f58c42f2f
Emily Daitch <emily.daitch@gmail.com>
2023-07-12 21:43:58 -0400

    style

--- components/contactFooter/index.tsx
+++ components/contactFooter/index.tsx
@@ -28,25 +28,25 @@ const ContactFooter = () => {
 				</Grid>
 				<Grid sx={footerStyle.root} item key={'link2'} xs={12} md={3} lg={3} paddingLeft={2} paddingRight={1}>
 					<Typography style={indexStyle.text1}>
-						<Link href='mailto:PGirvan@kw.com'>
+						<Link color='#000000' href='mailto:PGirvan@kw.com'>
 							<Mail /><br/>PGirvan@kw.com
 						</Link>
 					</Typography>
 					<Typography style={indexStyle.text1}>
-						<Link href='https://www.facebook.com/paulaandashleyhomes/'>
+						<Link color='#000000' href='https://www.facebook.com/paulaandashleyhomes/'>
 							<Facebook /><br/>Follow us on Facebook!
 						</Link>
 					</Typography>
 					<Typography style={indexStyle.text1}>
-						<Link href='https://www.instagram.com/paula.ashley.homes/'>
+						<Link color='#000000' href='https://www.instagram.com/paula.ashley.homes/'>
 							<Instagram /><br/>Follow us on Instagram!
 						</Link>
 					</Typography>
 				</Grid>
 				<Grid sx={footerStyle.root} item key={'addy'} xs={12} md={12} lg={12} paddingLeft={5} paddingRight={5}>
-					<Typography>315 W Ponce de Leon Ave Suite 100, Decatur, GA 30030</Typography>
+					<Typography style={indexStyle.text1}>315 W Ponce de Leon Ave Suite 100, Decatur, GA 30030</Typography>
 					<Link href='https://goo.gl/maps/QN7E6Eh1g5xYhXd48?coh=178571&entry=tt'>
-						<Typography sx={{textAlign: 'center'}}>
+						<Typography style={indexStyle.text1}>
 							<Google/><br/>We would love to hear your feedback, write a review!
 						</Typography>
 					</Link>
